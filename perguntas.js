5723810b55fec44fe9e3818982a4762ff4d0a4b3
daf08 <dafne.horvatich@escola.pr.gov.br>
2025-07-07 17:14:21 -0300

    Quimica

---
+++ perguntas.js
@@ -0,0 +1,29 @@
+criaCartao(
+    'Química',
+    'O que é uma ligação iônica?',
+    'É uma ligação entre metais e ametais, com transferência de elétrons.'
+)
+
+criaCartao(
+    'Química',
+    'Como se classificam os elementos na Tabela Periódica?',
+    'Em metais, ametais e gases nobres, organizados por número atômico crescente.'
+)
+
+criaCartao(
+    'Química',
+    'O que é uma reação de neutralização?',
+    'É uma reação entre um ácido e uma base que forma sal e água.'
+)
+
+criaCartao(
+    'Química',
+    'Qual é a fórmula da água e o tipo de ligação presente nela?',
+    'H₂O; apresenta ligações covalentes entre os átomos.'
+)
+
+criaCartao(
+    'Química',
+    'O que é mol na Química?',
+    'É a unidade que representa 6,02 × 10²³ partículas (átomos, moléculas ou íons).'
+)
